17caa60a1107fddc97287e1ca900ec3ae9ffd274
chris <cancerchris88@gmail.com>
2021-12-29 16:47:56 +0000

    brought in new version from the app dev template.

--- utilities/projectManagement/projectShutdown.m
+++ utilities/projectManagement/projectShutdown.m
@@ -9,10 +9,10 @@ clc;
 % Close down project
 disp('Closing down project.');
 
+% Clear workspace
+clear variables global
+disp('... Cleared workspace');
+
 % Close any open data dictionaries
 Simulink.data.dictionary.closeAll;
-disp('Closed data dictionary(s)');
-
-% Reset the location where generated code and other temporary files are
-% created (slprj) to the default:
-Simulink.fileGenControl('reset');
+disp('... Closed data dictionary(s)');
